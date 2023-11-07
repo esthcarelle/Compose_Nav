d0aa015da41dc37ff0fd9ede560b1b373f6e8824
Esther Carrelle <esthcarelle@gmail.com>
2023-11-07 12:54:19 +0200

    Added arguments to the Single Account screen

--- app/src/main/java/com/example/compose/rally/RallyActivity.kt
+++ app/src/main/java/com/example/compose/rally/RallyActivity.kt
@@ -91,7 +91,10 @@ fun RallyApp() {
                 composable(route = Bills.route) {
                     BillsScreen()
                 }
-                composable(route = SingleAccount.route) {
+                composable(
+                    route =
+                    "${SingleAccount.route}/{${SingleAccount.accountTypeArg}}"
+                ) {
                     SingleAccountScreen()
                 }
             }
