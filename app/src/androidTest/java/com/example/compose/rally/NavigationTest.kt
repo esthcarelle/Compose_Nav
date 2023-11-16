532ff33668104493dc17af388f5035a5fbfdfd22
Esther Carrelle <esthcarelle@gmail.com>
2023-11-16 10:28:57 +0200

    Test case that checks there is a click on All Bills

--- app/src/androidTest/java/com/example/compose/rally/NavigationTest.kt
+++ app/src/androidTest/java/com/example/compose/rally/NavigationTest.kt
@@ -6,8 +6,10 @@ import androidx.compose.ui.test.assertIsDisplayed
 import androidx.compose.ui.test.junit4.createComposeRule
 import androidx.compose.ui.test.onNodeWithContentDescription
 import androidx.compose.ui.test.performClick
+import androidx.compose.ui.test.performScrollTo
 import androidx.navigation.compose.ComposeNavigator
 import androidx.navigation.testing.TestNavHostController
+import junit.framework.Assert.assertEquals
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
@@ -47,5 +49,15 @@ class NavigationTest {
         composeTestRule
             .onNodeWithContentDescription("Accounts Screen")
             .assertIsDisplayed()
+    }
+
+    @Test
+    fun rallyNavHost_clickAllBills_navigateToBills() {
+        composeTestRule.onNodeWithContentDescription("All Bills")
+            .performScrollTo()
+            .performClick()
+
+        val route = navController.currentBackStackEntry?.destination?.route
+        assertEquals(route, "bills")
     }
 }
